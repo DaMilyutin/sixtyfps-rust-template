fd20668b43209870f4b9f4ba41c4b9174f25cbff
dmilyutin <dmilyutin@topcon.com>
2022-01-02 03:48:51 +0300

    * with leftovers

--- src/id_map_data.rs
+++ src/id_map_data.rs
@@ -1,7 +1,11 @@
 use super::*;
-use sixtyfps::Model;
 use std::rc::Rc;
 use std::{collections::HashMap, vec::Vec};
+
+use futures::future::{Fuse, FusedFuture, FutureExt};
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+
+use sixtyfps::{ComponentHandle, Model, ModelHandle, SharedString, VecModel};
 
 pub fn nextId() -> i32 {
     unsafe {
@@ -12,39 +16,33 @@ pub fn nextId() -> i32 {
     }
 }
 
-enum IdMapMessage {
-    PushId(i32),
-    SetProgress(f32),
-    RemoveId(i32),
-}
-
 pub struct IdMapData {
     pub id2row: HashMap<i32, usize>,
     pub row2id: Vec<i32>,
-    pub data: Rc<sixtyfps::VecModel<ListItemData>>,
+    //pub data: Rc<sixtyfps::VecModel<ListItemData>>,
 }
 
 impl IdMapData {
-    pub fn default() -> IdMapData {
+    pub fn new(data: Rc<sixtyfps::VecModel<ListItemData>>) -> IdMapData {
         IdMapData {
             id2row: HashMap::<i32, usize>::default(),
             row2id: Vec::<i32>::default(),
-            data: Rc::new(sixtyfps::VecModel::default()),
+            //data: data,
         }
     }
 
-    pub fn push(&mut self, id: i32) {
+    pub fn push(&mut self, data: &sixtyfps::VecModel<ListItemData>, id: i32) {
         self.row2id.push(id);
         self.id2row.insert(id, self.row2id.len() - 1);
-        self.data.push(ListItemData {
+        data.push(ListItemData {
             id: id,
             progress: 0f32,
         })
     }
 
-    pub fn set_progress(&mut self, id: i32, progress: f32) {
+    pub fn set_progress(&self, data: &sixtyfps::VecModel<ListItemData>, id: i32, progress: f32) {
         let row = *self.id2row.get(&id).unwrap();
-        self.data.set_row_data(
+        data.set_row_data(
             row,
             ListItemData {
                 id: id,
@@ -53,12 +51,12 @@ impl IdMapData {
         );
     }
 
-    pub fn remove_by_id(&mut self, id: i32) {
+    pub fn remove_by_id(&mut self, data: &sixtyfps::VecModel<ListItemData>, id: i32) {
         let row = *self.id2row.get(&id).unwrap();
-        self.remove_by_row(row);
+        self.remove_by_row(data, row);
     }
 
-    pub fn remove_by_row(&mut self, row: usize) {
+    pub fn remove_by_row(&mut self, data: &sixtyfps::VecModel<ListItemData>, row: usize) {
         assert!(0 <= row && row < self.row2id.len());
         let mut r = row + 1;
         while r < self.row2id.len() {
@@ -68,6 +66,84 @@ impl IdMapData {
         }
         self.id2row.remove(&self.row2id.get(row).unwrap());
         self.row2id.remove(row);
-        self.data.remove(row);
+        data.remove(row);
+    }
+
+    pub fn quit(&mut self, data: &sixtyfps::VecModel<ListItemData>) {
+        let mut r = self.row2id.len();
+        while r != 0 {
+            self.remove_by_row(data, r - 1);
+        }
     }
 }
+
+pub enum IdMapMessage {
+    PushId { id: i32 },
+    SetProgress { id: i32, progress: f32 },
+    RemoveId { id: i32 },
+    CancelId { id: i32 },
+    Quit,
+}
+
+// pub struct IdMapWorker {
+//     pub channel: UnboundedSender<IdMapMessage>,
+//     id_map_data: Arc<Mutex<IdMapData>>,
+//     worker_thread: std::thread::JoinHandle<()>,
+// }
+
+// impl IdMapWorker {
+//     pub fn new(cargo_ui: &AppWindow, data: Arc<Mutex<IdMapData>>) -> Self {
+//         let (channel, r) = tokio::sync::mpsc::unbounded_channel();
+//         let worker_thread = std::thread::spawn({
+//             let handle_weak = cargo_ui.as_weak();
+//             move || {
+//                 tokio::runtime::Runtime::new()
+//                     .unwrap()
+//                     .block_on(progress_worker_loop(r, handle_weak, data.clone()))
+//                     .unwrap()
+//             }
+//         });
+//         Self {
+//             channel,
+//             id_map_data: data.clone(),
+//             worker_thread,
+//         }
+//     }
+
+//     pub fn join(self) -> std::thread::Result<()> {
+//         let _ = self.channel.send(IdMapMessage::Quit);
+//         self.worker_thread.join()
+//     }
+// }
+
+// async fn progress_worker_loop(
+//     mut r: UnboundedReceiver<IdMapMessage>,
+//     handle: sixtyfps::Weak<AppWindow>,
+//     data: Arc<Mutex<IdMapData>>,
+// ) -> tokio::io::Result<()> {
+//     //let run_app = Fuse::terminated();
+//     //futures::pin_mut!(run_app);
+
+//     loop {
+//         let m = futures::select! {
+//             m = r.recv().fuse() => {
+//                 match m {
+//                     None => return Ok(()),
+//                     Some(m) => m,
+//                 }
+//             }
+//         };
+
+//         match m {
+//             IdMapMessage::PushId { id } => sixtyfps::invoke_from_event_loop({
+//                 let wh = handle.clone();
+//                 let wd = data.clone();
+//                 move || {
+//                     wd.get_mut().push(id);
+//                 }
+//             }),
+//             IdMapMessage::Quit => return Ok(()),
+//             _ => return Ok(()),
+//         };
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 sixtyfps::include_modules!();
 
-use sixtyfps::Model;
-use std::borrow::Borrow;
+use sixtyfps::{Model, VecModel};
+use std::borrow::{Borrow, BorrowMut};
+use std::ops::Deref;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time;
@@ -17,23 +18,70 @@ fn main() {
 
     let ui_handle = ui.as_weak();
 
-    let mut task_data = Rc::new(RefCell::new(IdMapData::default()));
-    ui.set_task_data_model(sixtyfps::ModelHandle::new(
-        task_data.as_ref().borrow().data.clone(),
-    ));
+    let list_data = Rc::new(VecModel::<ListItemData>::default());
+    let task_data = Arc::new(Mutex::new(IdMapData::new(list_data.clone())));
+
+    ui.set_task_data_model(sixtyfps::ModelHandle::new(list_data.clone()));
 
     ui.on_request_increase_value({
-        let mut task_data = task_data.clone();
+        let task_data = task_data.clone();
+        let model_handle = ui.get_task_data_model();
         move || {
             let ui = ui_handle.unwrap();
             let id = nextId() as i32;
-            (*task_data.borrow_mut()).push(id);
+            let model_handle = ui.get_task_data_model();
+            let data = model_handle
+                .as_any()
+                .downcast_ref::<VecModel<ListItemData>>()
+                .unwrap();
+            task_data.lock().as_deref_mut().unwrap().push(data, id);
             let latency = ui.get_latency();
-            let period = time::Duration::from_millis(((10.0 as f32) * latency) as u64);
-            ui.set_counter(ui.get_counter() + 1);
+            let period = time::Duration::from_millis(10);
+
+            let ui_handle = ui.as_weak();
+
+            let task_data = task_data.clone();
+
+            std::thread::spawn(move || {
+                for i in 0..101 {
+                    thread::sleep(period);
+                    let task_data = task_data.clone();
+                    let ui_handle = ui_handle.clone();
+                    sixtyfps::invoke_from_event_loop(move || {
+                        let ui = ui_handle.unwrap();
+                        let model_handle = ui.get_task_data_model();
+                        let data = model_handle
+                            .as_any()
+                            .downcast_ref::<VecModel<ListItemData>>()
+                            .unwrap();
+                        task_data
+                            .lock()
+                            .as_deref_mut()
+                            .unwrap()
+                            .set_progress(data, id, i as f32)
+                    });
+                }
+                thread::sleep(time::Duration::from_secs(1));
+                sixtyfps::invoke_from_event_loop(move || {
+                    let ui = ui_handle.unwrap();
+                    let model_handle = ui.get_task_data_model();
+                    let data = model_handle
+                        .as_any()
+                        .downcast_ref::<VecModel<ListItemData>>()
+                        .unwrap();
+                    task_data
+                        .lock()
+                        .as_deref_mut()
+                        .unwrap()
+                        .remove_by_id(data, id);
+                    ui.set_counter(ui.get_counter() + 1);
+                });
+            });
+
             //thread::sleep(time::Duration::from_secs(1));
             //task_data.lock().unwrap().remove_by_id(id);
         }
     });
+
     ui.run();
 }
